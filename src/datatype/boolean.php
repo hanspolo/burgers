ced32e31f0626c90e22f55cf484df955ca7b7bb4
hanspolo <ph.hanspolo@googlemail.com>
2013-06-03 11:14:47 +0200

    Updated formgeneration for boolean and integer

--- src/datatype/boolean.php
+++ src/datatype/boolean.php
@@ -8,4 +8,21 @@ class Boolean extends DataType
   {
     return is_bool($value);
   }
+
+  /**
+   *  @see \datatype\DateType::renderForm($name, $value)
+   */
+  public function renderForm($name, $value = null)
+  { 
+    $f3 = \Base::instance();
+
+    $form = "<label class=\"checkbox\">";
+    $form .= "<input name=\"$name\" type=\"checkbox\" ";
+    if ($value === true) $form .= "checked=\"checked\"";
+    $form .=">";
+    $form .= $f3->exists("lng.$name") ? $f3->get("lng.$name") : $name;
+    $form .= "</label>";
+
+    return $form;
+  }
 }
--- src/datatype/integer.php
+++ src/datatype/integer.php
@@ -8,4 +8,20 @@ class Integer extends DataType
   {
     return is_int($value);
   }
+
+  /**
+   *  @see \datatype\DateType::renderForm($name, $value)
+   */
+  public function renderForm($name, $value = null)
+  { 
+    $f3 = \Base::instance();
+
+    $form = "<label>";
+    $form .= $f3->exists("lng.$name") ? $f3->get("lng.$name") : $name;
+    $form .= "</label>";
+    $form .= "<input name=\"$name\" type=\"number\" value=\"$value\">";
+
+    return $form;
+  }
+
 }
